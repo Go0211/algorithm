c301ee93b94091a03e3ca3a6457883df1e77c3b0
rhtjdxo <v12rhtjdxo@naver.com>
2024-10-28 08:49:17 +0900

    1028 문제제출

---
+++ algorithm_problem/day1028/Back6148.java
@@ -0,0 +1,40 @@
+package day1028;
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Back6148 {
+	static int answer = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int seq = Integer.parseInt(st.nextToken());
+		int limit = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[seq];
+		for (int i = 0; i < seq; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		comb(arr, 0, 0, limit, new boolean[seq]);
+		
+		System.out.println(answer);
+	}
+
+	private static void comb(int[] arr, int depth, int value, int limit, boolean[] v) {
+		if (limit <= value) {
+			answer = Math.min(value - limit, answer);
+			return;
+		}
+		
+		for (int i = depth; i < arr.length; i++) {
+			if(!v[i]) {
+				v[i] = true;
+				comb(arr, i + 1, value + arr[i], limit, v);
+				v[i] = false;
+			}
+		}
+	}
+}
