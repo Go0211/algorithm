87f5b0b1812a94c202423d366134bf293ab50c59
Go0211 <67907707+Go0211@users.noreply.github.com>
2025-04-13 18:37:14 +0900

    [Bronze III] Title: 학술대회 참가신청, Time: 68 ms, Memory: 11528 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.java
+++ 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.java
@@ -7,8 +7,17 @@ public class Main {
             new BufferedReader(new InputStreamReader(System.in));
         List<String> l1 = Arrays.asList("social", "history", "language", "literacy");
         List<String> l2 = Arrays.asList("bigdata", "public", "society");
+        String str = br.readLine();
+        boolean t = true;
         
-        if(l1.contains(br.readLine())) {
+        for(String s : l1) {
+            if(str.contains(s)) {
+                t = false;
+                break;
+            }
+        }
+        
+        if(!t) {
             System.out.println("digital humanities");
         } else {
             System.out.println("public bigdata");
