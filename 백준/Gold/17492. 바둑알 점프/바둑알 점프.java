a201dd5c1903bad0092c608febaa1153931f90a1
Go0211 <67907707+Go0211@users.noreply.github.com>
2025-04-27 12:31:49 +0900

    [Gold IV] Title: 바둑알 점프, Time: 76 ms, Memory: 11796 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17492. 바둑알 점프/바둑알 점프.java
+++ 백준/Gold/17492. 바둑알 점프/바둑알 점프.java
@@ -2,6 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static class Node {
+        int y, x;
+        Node(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Node node = (Node) obj;
+            return y == node.y && x == node.x;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(y, x);
+        }
+    }
+    
     static int[][] dirs = 
         {
             {1, 0},{1, 1},{0, 1},
@@ -16,7 +37,7 @@ public class Main {
             new BufferedReader(new InputStreamReader(System.in));
         int len = Integer.parseInt(br.readLine());
         int[][] map = new int[len][len];
-        List<int[]> stones = new ArrayList<>();
+        List<Node> stones = new ArrayList<>();
         for(int i = 0; i < len; i++) {
             StringTokenizer st =
                 new StringTokenizer(br.readLine(), " ");
@@ -24,47 +45,51 @@ public class Main {
                 int num = Integer.parseInt(st.nextToken());
                 map[i][j] = num;
                 if(num == 2) {
-                    stones.add(new int[]{i, j});
+                    stones.add(new Node(i, j));
                 }
             }
         }
         
-        for(int[] ints : stones) {
-            if(stones.size() == 1) {
-                answer = "Possible";
-                break;
-            }
-            if(answer.equals("Possible")) {
-                break;
-            }
-            
-            dfs(map, ints[0], ints[1], len, stones.size() - 1);
-        }
+        dfs(map, stones, len, stones.size() - 1);
         
         System.out.println(answer);
     }
     
-    static void dfs(int[][] map, int y, int x, int len, int move) {
+    static void dfs(int[][] map, List<Node> stones, int len, int move) {
         if(move == 0) {
             answer = "Possible";
             return;
         }
         
-        for(int[] dir : dirs) {
-            if(answer.equals("Possible")) {
-                return;
+        next:
+        for(int i = 0; i < stones.size(); i++) {
+            for(int[] dir : dirs) {
+                if(answer.equals("Possible")) {
+                    return;
+                }
+                
+                int ny = stones.get(i).y + dir[0];
+                int nx = stones.get(i).x + dir[1];
+                int nny = ny + dir[0];
+                int nnx = nx + dir[1];
+            
+                if(map[stones.get(i).y][stones.get(i).x] == 2 && map[ny][nx] == 2 && map[nny][nnx] == 0) {
+                    List<Node> clone = new ArrayList<>(stones);
+                    
+                    map[stones.get(i).y][stones.get(i).x] = 0;
+                    map[ny][nx] = 0;
+                    map[nny][nnx] = 2;
+                    
+                    clone.add(new Node(nny, nnx));
+                    
+                    dfs(map, clone, len, move - 1);
+                    map[stones.get(i).y][stones.get(i).x] = 2;
+                    map[ny][nx] = 2;
+                    map[nny][nnx] = 0;
+                    // stones.remove(n);
+                    // stones.add(nn);
+                }  
             }
-            
-            int ny = y + dir[0];
-            int nx = x + dir[1];
-            int nny = ny + dir[0];
-            int nnx = nx + dir[1];
-            
-            if(map[ny][nx] == 2 && map[nny][nnx] == 0) {
-                map[ny][nx] = 0;
-                dfs(map, nny, nnx, len, move - 1);
-                map[ny][nx] = 2;
-            }  
         }
     }
 }
