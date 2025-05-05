5c8ef3757c3d6420e754f198cb30db2775505e0e
Go0211 <67907707+Go0211@users.noreply.github.com>
2025-05-05 14:08:56 +0900

    [Silver I] Title: 완전 범죄, Time: 124 ms, Memory: 15560 KB -BaekjoonHub

--- 백준/Silver/13700. 완전 범죄/완전 범죄.java
+++ 백준/Silver/13700. 완전 범죄/완전 범죄.java
@@ -18,40 +18,52 @@ public class Main {
     Arrays.fill(arr, Integer.MAX_VALUE);
 
     if(policeCount != 0) {
-        st = new StringTokenizer(br.readLine());
+      st = new StringTokenizer(br.readLine());
     }
     for(int i = 0; i < policeCount; i++) {
       arr[Integer.parseInt(st.nextToken()) - 1] = -1;
     }
 
     arr[start] = 0;
-    dfs(arr, 0, start, front, back, home, len);
+    int result = bfs(arr, start, front, back, home, len);
 
     System.out.println(
-        arr[home] == Integer.MAX_VALUE ?
-            "BUG FOUND" : arr[home]
+        result == Integer.MAX_VALUE ?
+            "BUG FOUND" : result
     );
   }
 
-  static void dfs(
-      int[] arr, int count, int start,
-      int front, int back, int home, int len)
+  static int bfs(
+      int[] arr, int start,
+      int front, int back,
+      int home, int len)
   {
-    if(home == start) {
-      return;
+    ArrayDeque<int[]> q = new ArrayDeque<>();
+    q.offer(new int[]{start, 0});
+
+    while(!q.isEmpty()) {
+      int[] ints = q.poll();
+      int preIdx = ints[0];
+      int preCount = ints[1];
+
+      if(preIdx == home) {
+        return preCount;
+      }
+
+      int fm = preIdx + front;
+      int bm = preIdx - back;
+
+      if(0 <= fm && fm < len && arr[fm] > preCount + 1) {
+        arr[fm] = preCount + 1;
+        q.offer(new int[]{fm, preCount + 1});
+      }
+
+      if(0 <= bm && bm < len && arr[bm] > preCount + 1) {
+        arr[bm] = preCount + 1;
+        q.offer(new int[]{bm, preCount + 1});
+      }
     }
 
-    int fm = start + front;
-    int bm = start - back;
-
-    if(0 <= fm && fm < len && arr[fm] > count + 1) {
-      arr[fm] = count + 1;
-      dfs(arr, count + 1, fm, front, back, home, len);
-    }
-
-    if(0 <= bm && bm < len && arr[bm] > count + 1) {
-      arr[bm] = count + 1;
-      dfs(arr, count + 1, bm, front, back, home, len);
-    }
+    return Integer.MAX_VALUE;
   }
 }
