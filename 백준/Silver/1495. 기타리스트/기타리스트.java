8e93e917cac8ad581fbe231b03c4332f5a66e5e4
Go0211 <67907707+Go0211@users.noreply.github.com>
2025-05-10 11:05:51 +0900

    [Silver I] Title: 기타리스트, Time: 64 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -13,30 +13,37 @@ public class Main {
         int start = Integer.parseInt(st.nextToken());
         int limit = Integer.parseInt(st.nextToken());
         int[] arr = new int[seq];
-        boolean[][] dp = new boolean[seq + 1][limit + 1];
-        dp[0][start] = true;
+        boolean[] preDP = new boolean[limit + 1];
+        boolean[] nextDP = new boolean[limit + 1];
+        preDP[start] = true;
         
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < seq; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         
-        for(int i = 0; i < seq; i++) {
-            for(int j = 0; j < limit + 1; j++) {
-                if(dp[i][j]) {
-                    if(j + arr[i] <= limit) {
-                        dp[i + 1][j + arr[i]] = true;
+        for(int z = 0; z < seq; z++) {
+            Arrays.fill(nextDP, false);
+            
+            for(int i = 0; i < limit + 1; i++) {
+                if(preDP[i]) {
+                    if(i + arr[z] <= limit) {
+                        nextDP[i + arr[z]] = true;
                     }
-                    if(j - arr[i] >= 0) {
-                        dp[i + 1][j - arr[i]] = true;
+                    if(i - arr[z] >= 0) {
+                        nextDP[i - arr[z]] = true;
                     }
                 }
             }
+            
+            boolean[] temp = nextDP;
+            nextDP = preDP;
+            preDP = temp;
         }
         
         int answer = -1;
         for(int j = limit; j >= 0; j--) {
-            if(dp[seq][j]) {
+            if(preDP[j]) {
                 answer = j;
                 break;
             }
