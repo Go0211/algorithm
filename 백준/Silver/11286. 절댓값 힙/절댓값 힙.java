49dbbd7f25f5233ac711a7f5040b5842b9121c32
Go0211 <67907707+Go0211@users.noreply.github.com>
2025-06-14 10:49:43 +0900

    [Silver I] Title: 절댓값 힙, Time: 256 ms, Memory: 23488 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,16 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+  static class Numbers implements Comparable<Numbers> {
+    int value;
+
+    Numbers(int value) {
+      this.value = value;
+    }
+
+    public int compareTo(Numbers other) {
+      if (Math.abs(value) == Math.abs(other.value)) {
+        return Integer.compare(value, other.value);
+      } else {
+        return Integer.compare(Math.abs(value), Math.abs(other.value));
+      }
+    }
+  }
+
   public static void main(String[] args) throws Exception {
     StringBuilder sb = new StringBuilder();
     BufferedReader br =
         new BufferedReader(new InputStreamReader(System.in));
-    PriorityQueue<Integer> pq =
-        new PriorityQueue<>((x, y) -> {
-          return Math.abs(x) == Math.abs(y)
-              ? (x > y ? 1 : -1)
-              : (Math.abs(x) > Math.abs(y) ? 1 : -1);
-        });
+    PriorityQueue<Numbers> pq = new PriorityQueue<>();
 
     int seq = Integer.parseInt(br.readLine());
 
@@ -19,11 +30,11 @@ public class Main {
       int num = Integer.parseInt(br.readLine());
 
       if(num == 0) {
-        sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+        sb.append(pq.isEmpty() ? 0 : pq.poll().value).append("\n");
         continue;
       }
 
-      pq.offer(num);
+      pq.offer(new Numbers(num));
     }
 
     System.out.print(sb);
