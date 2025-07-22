38d05656b6e3d0183997a5937c256c8ad42b99de
Go0211 <67907707+Go0211@users.noreply.github.com>
2025-07-22 23:45:56 +0900

    [Gold III] Title: 두 배, Time: 300 ms, Memory: 43300 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.java
+++ 백준/Gold/31963. 두 배/두 배.java
@@ -1,32 +1,35 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br =
-                new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        long[] arr = new long[N];
-        int answer = 0;
+  public static void main(String[] args) throws Exception {
+    BufferedReader  br =
+        new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st =
-                new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
-        }
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[N];
 
-        for(int i = 1; i < N; i++) {
-            long front = arr[i - 1];
-            long target = arr[i];
-
-            while(front > target) {
-                target *= 2;
-                answer++;
-            }
-
-            arr[i] = target;
-        }
-
-        System.out.println(answer);
+    StringTokenizer st =
+        new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+    System.out.println(sol(arr, N));
+  }
+
+  public static long sol(int[] arr, int n) {
+    long tmp = 0;
+    long[] cnt_arr = new long[n];
+    for (int i = 1; i < n; i++) {
+      double ratio =
+          Math.ceil(Math.log(arr[i - 1]
+              / (double)arr[i]) / Math.log(2)) + cnt_arr[i - 1];
+      if (ratio > 0) {
+        cnt_arr[i] = Math.max(0, (long)ratio);
+        tmp += cnt_arr[i];
+      }
+    }
+    return tmp;
+  }
 }
