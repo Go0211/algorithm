2408492f2d3a84b5228869204f69d6abaa38d4da
Go0211 <67907707+Go0211@users.noreply.github.com>
2025-08-29 13:33:24 +0900

    [Gold III] Title: 세 용액, Time: 140 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -3,41 +3,41 @@ import java.util.*;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = 
-        new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    int[] a = new int[N];
-    StringTokenizer st = 
-        new StringTokenizer(br.readLine());
-    for (int i = 0; i < N; i++) {
-      a[i] = Integer.parseInt(st.nextToken());
-    }
-    Arrays.sort(a);
-
-    long best = Long.MAX_VALUE;
-    int rx = 0, ry = 1, rz = 2;
-
-    for (int i = 0; i < N - 2; i++) {
-      int l = i + 1, r = N - 1;
-      while (l < r) {
-        long sum = (long) a[i] + a[l] + a[r];
-        if (Math.abs(sum) < Math.abs(best)) {
-          best = sum;
-          rx = i;
-          ry = l;
-          rz = r;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] a = new int[N];
+        StringTokenizer st =
+                new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
         }
-        if (sum > 0) {
-          r--;
-        } else if (sum < 0) {
-          l++;
-        } else {
-          System.out.println(a[rx] + " " + a[ry] + " " + a[rz]);
-          return;
+        Arrays.sort(a);
+
+        long best = Long.MAX_VALUE;
+        int x = 0, y = 1, z = 2;
+
+        for (int i = 0; i < N - 2; i++) {
+            int start = i + 1, end = N - 1;
+            while (start < end) {
+                long sum = (long) a[i] + a[start] + a[end];
+                if (Math.abs(sum) < Math.abs(best)) {
+                    best = sum;
+                    x = i;
+                    y = start;
+                    z = end;
+                }
+                if (sum > 0) {
+                    end--;
+                } else if (sum < 0) {
+                    start++;
+                } else {
+                    System.out.println(a[x] + " " + a[y] + " " + a[z]);
+                    return;
+                }
+            }
         }
-      }
+        System.out.println(a[x] + " " + a[y] + " " + a[z]);
     }
-    System.out.println(a[rx] + " " + a[ry] + " " + a[rz]);
-  }
 }
