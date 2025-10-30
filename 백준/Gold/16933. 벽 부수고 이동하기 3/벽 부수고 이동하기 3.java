4e4aabe526fa8bf64a100700d6d827c9042fbab5
Go0211 <67907707+Go0211@users.noreply.github.com>
2025-10-30 11:05:05 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 4084 ms, Memory: 431324 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -4,22 +4,6 @@ import java.util.*;
 public class Main {
     static int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
-    static class Node {
-        int y, x, k, d, count;
-
-        Node(int y, int x, int k, int d, int count) {
-            this.y = y;
-            this.x = x;
-            this.k = k;
-            this.d = d;
-            this.count = count;
-        }
-
-        public String toString() {
-            return "y:"+y+" x:"+x+" k:"+k+" d:"+d +" count:" + count;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br =
                 new BufferedReader(new InputStreamReader(System.in));
@@ -37,17 +21,17 @@ public class Main {
             }
         }
 
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(0, 0, K, 1, 0));
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0, 0, K, 1, 0});
         boolean[][][][] v = new boolean[R][C][2][K + 1];
         int answer = Integer.MAX_VALUE;
         while(!queue.isEmpty()) {
-            Node node = queue.poll();
-            int py = node.y;
-            int px = node.x;
-            int pk = node.k;
-            int pd = node.d;
-            int count = node.count;
+            int[] node = queue.poll();
+            int py = node[0];
+            int px = node[1];
+            int pk = node[2];
+            int pd = node[3];
+            int count = node[4];
 
             if(py == R - 1 && px == C - 1) {
                 answer = count;
@@ -61,17 +45,17 @@ public class Main {
                 if(ny >= 0 && ny < R && nx >= 0 && nx < C) {
                     // 낮 + 벽X or 밤 + 벽X
                     if(sample[ny][nx] && !v[ny][nx][(pd + 1) % 2][pk]) {
-                        queue.offer(new Node(ny, nx, pk, (pd + 1) % 2, count + 1));
+                        queue.offer(new int[]{ny, nx, pk, (pd + 1) % 2, count + 1});
                         v[ny][nx][(pd + 1) % 2][pk] = true;
                     }
                     // 낮 + 벽0
                     else if(!sample[ny][nx] && pk > 0 && pd == 1 && !v[ny][nx][0][pk - 1]) {
-                        queue.offer(new Node(ny, nx, pk - 1, 0, count + 1));
+                        queue.offer(new int[]{ny, nx, pk - 1, 0, count + 1});
                         v[ny][nx][0][pk - 1] = true;
                     }
                     // 밤 + 벽0
                     else if(!sample[ny][nx] && pk > 0 && pd == 0) {
-                        queue.offer(new Node(py, px, pk, 1, count + 1));
+                        queue.offer(new int[]{py, px, pk, 1, count + 1});
                         v[py][px][pd][pk] = true;
                     }
                 }
